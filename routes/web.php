afba55692a8511b2b3865f68741cef59db9b9ebf
jota.kaneko <sarukiti0418@outlook.jp>
2020-11-14 00:27:04 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get("logout","Auth\LoginController@logout")->name("logout.get");
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
