6753bd1e21af55d8959c81369dcff5364036dd63
jota.kaneko <sarukiti0418@outlook.jp>
2020-11-21 16:09:48 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -10,8 +10,10 @@ class MicropostsController extends Controller
     {
         $data=[];
         if (\Auth::check()){
+            //認証済みのユーザを取得
             $user=\Auth::user();
-            $microposts=$user->microposts()->orderBy("created_at","desc")->paginate(10);
+            // ユーザとフォロー中ユーザの投稿の一覧を作成日時の降順で取得
+            $microposts=$user->feed_microposts()->orderBy("created_at","desc")->paginate(10);
             
             $data=[
                 "user"=>$user,
--- app/User.php
+++ app/User.php
@@ -94,6 +94,19 @@ class User extends Authenticatable
          //フォロー中のユーザー内に$useridのものが存在するか
          return $this->followings()->where("follow_id",$userId)->exists();
      }
+     
+         /**
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
+     */
+     public function feed_microposts()
+     {
+         //このユーザーがフォロー中のユーザのidを取得して配列にする
+         $userIds = $this->followings()->pluck("users.id")->toArray();
+         //このユーザーのidも配列に追加
+         $userIds[] =$this->id;
+         //それらのユーザーが所有する投稿に絞り込む
+         return Micropost::whereIn("user_id",$userIds);
+     }
 
 
 
